0ab82686214bf7c9811fded9ce422262f3de558a
Renaldy <renaldytherrydota@gmail.com>
2023-06-04 12:54:58 +0800

    add some changes and readme.md

--- README.md
+++ README.md
@@ -1,9 +1,60 @@
-# BCR API
+# Challenge Chapter 7
+percobaan unit testing menggunakan jest dan supertest, dan percobaan deployment menggunakan railway
+## Data Admin
 
-Di dalam repository ini terdapat implementasi API dari Binar Car Rental.
-Tugas kalian disini adalah:
-1. Fork repository
-2. Tulis unit test di dalam repository ini menggunakan `jest`.
-3. Coverage minimal 70%
+-   email : renaldy@binar.co.id
+-   password : "123456"
 
-Good luck!
+## Setup
+
+1. Install Library
+
+```bash
+yarn install
+```
+
+2. Create an .env file on root folder
+
+```bash
+JWT_SIGNATURE_KEY=Rahasia
+DB_USER=
+DB_PASSWORD=
+DB_NAME=
+DB_HOST=
+DB_PORT=
+```
+
+3. Create Database
+
+```bash
+yarn db:create
+```
+
+4. Migrating the model
+
+```bash
+yarn db:migrate
+```
+
+5. Using seeder
+
+```bash
+yarn db:seed
+```
+
+## How To Test
+
+-   Testing the app
+    -   it will execute pre-script command to run on test mode
+
+```bash
+yarn test
+```
+
+## How To Run
+
+-   Running Project on development
+
+```bash
+yarn develop
+```
--- db/seeders/20220528064914-users.js
+++ db/seeders/20220528064914-users.js
@@ -1,47 +1,42 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
-const { Op } = require("sequelize");
-const bcrypt = require("bcryptjs");
-const { Role } = require("../../app/models");
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const { Role } = require('../../app/models');
 
-const names = [
-  "Johnny",
-  "Fikri",
-  "Brian",
-  "Ranggawarsita",
-  "Jayabaya",
-]
+const names = ['Johnny', 'Fikri', 'Brian', 'Ranggawarsita', 'Jayabaya', 'Arief'];
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    const password = "123456";
-    const encryptedPassword = bcrypt.hashSync(password, 10);
-    const timestamp = new Date();
+    async up(queryInterface, Sequelize) {
+        const password = '123456';
+        const encryptedPassword = bcrypt.hashSync(password, 10);
+        const timestamp = new Date();
 
-    const customerRole = await Role.findOne({
-      where: {
-        name: "CUSTOMER",
-      },
-    });
-    const adminRole = await Role.findOne({
-       where: {
-        name:'ADMIN',
-       },
-    });
+        const roleCustomer = await Role.findOne({
+            where: {
+                name: 'CUSTOMER',
+            },
+        });
+        const roleAdmin = await Role.findOne({
+            where: {
+                name: 'ADMIN',
+            },
+        });
 
-    const users = names.map((name) => ({
-      name,
-      email: `${name.toLowerCase()}@binar.co.id`,
-      encryptedPassword,
-      roleId: name.toLowerCase() === 'renaldy' ? adminRole.id : customerRole.id,
-      createdAt: timestamp,
-      updatedAt: timestamp,
-    }))
+        const users = names.map((name) => ({
+            name,
+            email: `${name.toLowerCase()}@binar.co.id`,
+            encryptedPassword,
+            roleId: name.toLowerCase() === 'renaldy' ? roleAdmin.id : roleCustomer.id,
+            createdAt: timestamp,
+            updatedAt: timestamp,
+        }));
 
-    await queryInterface.bulkInsert('Users', users, {});
-  },
+        await queryInterface.bulkInsert('Users', users, {});
+    },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Users', { name: { [Op.in]: names } }, {});
-  }
+    async down(queryInterface, Sequelize) {
+        await queryInterface.bulkDelete('Users', { name: { [Op.in]: names } }, {});
+    },
 };
